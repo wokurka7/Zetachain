3668d6b93e17f503eba6c33b121ade582e094d65
lumtis <lucas.bertrand.22@gmail.com>
2024-01-29 13:08:30 -0800

    revert back stress test change

--- cmd/zetae2e/local/bitcoin.go
+++ cmd/zetae2e/local/bitcoin.go
@@ -2,16 +2,13 @@ package local
 
 import (
 	"fmt"
-	ethtypes "github.com/ethereum/go-ethereum/core/types"
-	"github.com/zeta-chain/zetacore/contrib/localnet/orchestrator/smoketest/utils"
-	crosschaintypes "github.com/zeta-chain/zetacore/x/crosschain/types"
-	"math/big"
 	"runtime"
 	"time"
 
 	"github.com/fatih/color"
 	"github.com/zeta-chain/zetacore/contrib/localnet/orchestrator/smoketest/config"
 	"github.com/zeta-chain/zetacore/contrib/localnet/orchestrator/smoketest/runner"
+	"github.com/zeta-chain/zetacore/contrib/localnet/orchestrator/smoketest/smoketests"
 )
 
 // bitcoinTestRoutine runs Bitcoin related smoke tests
@@ -48,7 +45,7 @@ func bitcoinTestRoutine(
 		}
 
 		bitcoinRunner.Logger.Print("🏃 starting Bitcoin tests")
-		//startTime :=  time.Now()
+		startTime := time.Now()
 
 		// funding the account
 		txUSDTSend := deployerRunner.SendUSDTOnEvm(UserBitcoinAddress, 1000)
@@ -64,97 +61,24 @@ func bitcoinTestRoutine(
 		bitcoinRunner.SetupBitcoinAccount(initBitcoinNetwork)
 		bitcoinRunner.DepositBTC(true)
 
-		tx, err := bitcoinRunner.BTCZRC20.Approve(bitcoinRunner.ZevmAuth, bitcoinRunner.BTCZRC20Addr, big.NewInt(1e18))
-		if err != nil {
-			panic(err)
+		// run bitcoin test
+		// Note: due to the extensive block generation in Bitcoin localnet, block header test is run first
+		// to make it faster to catch up with the latest block header
+		if err := bitcoinRunner.RunSmokeTestsFromNames(
+			smoketests.AllSmokeTests,
+			smoketests.TestBitcoinWithdrawName,
+			smoketests.TestSendZetaOutBTCRevertName,
+			smoketests.TestCrosschainSwapName,
+		); err != nil {
+			return fmt.Errorf("bitcoin tests failed: %v", err)
 		}
-		receipt := utils.MustWaitForTxReceipt(bitcoinRunner.Ctx, bitcoinRunner.ZevmClient, tx, bitcoinRunner.Logger, bitcoinRunner.ReceiptTimeout)
-		if receipt.Status != 1 {
-			panic(fmt.Errorf("approve receipt status is not 1"))
+
+		if err := bitcoinRunner.CheckBtcTSSBalance(); err != nil {
+			return err
 		}
 
-		bitcoinRunner.MineBlocks()
+		bitcoinRunner.Logger.Print("🍾 Bitcoin tests completed in %s", time.Since(startTime).String())
 
-		go WithdrawCCtx(bitcoinRunner)
-
-		//// run bitcoin test
-		//// Note: due to the extensive block generation in Bitcoin localnet, block header test is run first
-		//// to make it faster to catch up with the latest block header
-		//if err := bitcoinRunner.RunSmokeTestsFromNames(
-		//	smoketests.AllSmokeTests,
-		//	smoketests.TestBitcoinWithdrawName,
-		//	//smoketests.TestSendZetaOutBTCRevertName,
-		//	//smoketests.TestCrosschainSwapName,
-		//); err != nil {
-		//	return fmt.Errorf("bitcoin tests failed: %v", err)
-		//}
-		//
-		//if err := bitcoinRunner.CheckBtcTSSBalance(); err != nil {
-		//	return err
-		//}
-		//
-		//bitcoinRunner.Logger.Print("🍾 Bitcoin tests completed in %s", time.Since(startTime).String())
-
-		time.Sleep(100 * time.Second)
-
-		return nil
+		return err
 	}
-}
-
-// TODO: Remove this part
-
-var (
-	zevmNonce = big.NewInt(1)
-)
-
-// WithdrawCCtx withdraw USDT from ZEVM to EVM
-func WithdrawCCtx(sm *runner.SmokeTestRunner) {
-	ticker := time.NewTicker(time.Second * 2)
-	for {
-		select {
-		case <-ticker.C:
-			WithdrawBTCZRC20(sm)
-		}
-	}
-}
-
-func WithdrawBTCZRC20(sm *runner.SmokeTestRunner) {
-	defer func() {
-		zevmNonce.Add(zevmNonce, big.NewInt(1))
-	}()
-
-	sm.ZevmAuth.Nonce = zevmNonce
-
-	sm.Logger.Print("nonce %d: starting withdraw", zevmNonce)
-	tx, err := sm.BTCZRC20.Withdraw(sm.ZevmAuth, []byte(sm.BTCDeployerAddress.EncodeAddress()), big.NewInt(100))
-	if err != nil {
-		panic(err)
-	}
-
-	go MonitorCCTXFromTxHash(sm, tx, zevmNonce.Int64())
-}
-
-func MonitorCCTXFromTxHash(sm *runner.SmokeTestRunner, tx *ethtypes.Transaction, nonce int64) {
-	receipt := utils.MustWaitForTxReceipt(sm.Ctx, sm.ZevmClient, tx, sm.Logger, sm.ReceiptTimeout)
-	if receipt.Status == 0 {
-		sm.Logger.Print("nonce %d: withdraw evm tx failed", nonce)
-		return
-	}
-	sm.Logger.Print("nonce %d: withdraw evm tx success, receipt: %+v", nonce, receipt)
-	// mine 10 blocks to confirm the withdraw tx
-	_, err := sm.BtcRPCClient.GenerateToAddress(10, sm.BTCDeployerAddress, nil)
-	if err != nil {
-		panic(err)
-	}
-	cctx := utils.WaitCctxMinedByInTxHash(sm.Ctx, tx.Hash().Hex(), sm.CctxClient, sm.Logger, sm.ReceiptTimeout)
-	if cctx.CctxStatus.Status != crosschaintypes.CctxStatus_OutboundMined {
-		sm.Logger.Print(
-			"nonce %d: withdraw cctx failed with status %s, message %s",
-			nonce,
-			cctx.CctxStatus.Status,
-			cctx.CctxStatus.StatusMessage,
-		)
-		return
-	}
-	sm.Logger.Print("nonce %d: withdraw cctx success", nonce)
 }
--- cmd/zetae2e/stress.go
+++ cmd/zetae2e/stress.go
@@ -4,27 +4,25 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"math/big"
 	"os"
 	"sort"
 	"time"
 
-	"github.com/fatih/color"
-	zetae2econfig "github.com/zeta-chain/zetacore/cmd/zetae2e/config"
-	"github.com/zeta-chain/zetacore/cmd/zetae2e/local"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/zeta-chain/zetacore/app"
-	"github.com/zeta-chain/zetacore/contrib/localnet/orchestrator/smoketest/runner"
-	"github.com/zeta-chain/zetacore/contrib/localnet/orchestrator/smoketest/utils"
-
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"github.com/zeta-chain/protocol-contracts/pkg/contracts/zevm/zrc20.sol"
+	"github.com/zeta-chain/zetacore/app"
+	zetae2econfig "github.com/zeta-chain/zetacore/cmd/zetae2e/config"
+	"github.com/zeta-chain/zetacore/cmd/zetae2e/local"
+	"github.com/zeta-chain/zetacore/contrib/localnet/orchestrator/smoketest/runner"
+	"github.com/zeta-chain/zetacore/contrib/localnet/orchestrator/smoketest/utils"
 	crosschaintypes "github.com/zeta-chain/zetacore/x/crosschain/types"
+	"google.golang.org/grpc"
 )
 
 const (
@@ -89,7 +87,42 @@ func StressTest(cmd *cobra.Command, _ []string) {
 		panic(err)
 	}
 
+	// Initialize clients ----------------------------------------------------------------
+	goerliClient, err := ethclient.Dial(conf.RPCs.EVM)
+	if err != nil {
+		panic(err)
+	}
+
+	bal, err := goerliClient.BalanceAt(context.TODO(), local.DeployerAddress, nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Deployer address: %s, balance: %d Wei\n", local.DeployerAddress.Hex(), bal)
+
+	grpcConn, err := grpc.Dial(conf.RPCs.ZetaCoreGRPC, grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+
+	cctxClient := crosschaintypes.NewQueryClient(grpcConn)
 	// -----------------------------------------------------------------------------------
+
+	// Wait for Genesis and keygen to be completed if network is local. ~ height 30
+	if stressTestArgs.network == "LOCAL" {
+		time.Sleep(20 * time.Second)
+		for {
+			time.Sleep(5 * time.Second)
+			response, err := cctxClient.LastZetaHeight(context.Background(), &crosschaintypes.QueryLastZetaHeightRequest{})
+			if err != nil {
+				fmt.Printf("cctxClient.LastZetaHeight error: %s", err)
+				continue
+			}
+			if response.Height >= 30 {
+				break
+			}
+			fmt.Printf("Last ZetaHeight: %d\n", response.Height)
+		}
+	}
 
 	// initialize context
 	ctx, cancel := context.WithCancel(context.Background())
@@ -98,12 +131,7 @@ func StressTest(cmd *cobra.Command, _ []string) {
 	if err != nil {
 		panic(err)
 	}
-	logger := runner.NewLogger(verbose, color.FgHiBlue, "setup")
-
-	if stressTestArgs.network == "LOCAL" {
-		logger.Print("⏳ wait 70s for genesis")
-		time.Sleep(70 * time.Second)
-	}
+	logger := runner.NewLogger(verbose, color.FgWhite, "setup")
 
 	// initialize smoke test runner
 	smokeTest, err := zetae2econfig.RunnerFromConfig(
@@ -121,9 +149,6 @@ func StressTest(cmd *cobra.Command, _ []string) {
 		panic(err)
 	}
 
-	// wait for keygen
-	local.WaitKeygenHeight(ctx, smokeTest.CctxClient, logger)
-
 	// setup TSS addresses
 	smokeTest.SetTSSAddresses()
 
@@ -137,9 +162,6 @@ func StressTest(cmd *cobra.Command, _ []string) {
 		// deposit on ZetaChain
 		smokeTest.DepositEther(false)
 		smokeTest.DepositZeta()
-		smokeTest.SetupBitcoinAccount(true)
-		smokeTest.DepositBTC(false)
-
 	} else if stressTestArgs.network == "TESTNET" {
 		ethZRC20Addr, err := smokeTest.SystemContract.GasCoinZRC20ByChainId(&bind.CallOpts{}, big.NewInt(5))
 		if err != nil {
@@ -163,23 +185,14 @@ func StressTest(cmd *cobra.Command, _ []string) {
 	fmt.Printf("eth zrc20 balance: %s Wei \n", ethZRC20Balance.String())
 
 	//Pre-approve ETH withdraw on ZEVM
-	fmt.Printf("approving ZRC20...\n")
-	tx, err := smokeTest.ETHZRC20.Approve(smokeTest.ZevmAuth, smokeTest.ETHZRC20Addr, big.NewInt(1e18))
+	fmt.Printf("approving ETH ZRC20...\n")
+	ethZRC20 := smokeTest.ETHZRC20
+	tx, err := ethZRC20.Approve(smokeTest.ZevmAuth, smokeTest.ETHZRC20Addr, big.NewInt(1e18))
 	if err != nil {
 		panic(err)
 	}
 	receipt := utils.MustWaitForTxReceipt(ctx, smokeTest.ZevmClient, tx, logger, smokeTest.ReceiptTimeout)
-	if receipt.Status == 0 {
-		panic("approve failed")
-	}
-	tx, err = smokeTest.BTCZRC20.Approve(smokeTest.ZevmAuth, smokeTest.BTCZRC20Addr, big.NewInt(1e18))
-	if err != nil {
-		panic(err)
-	}
-	receipt = utils.MustWaitForTxReceipt(ctx, smokeTest.ZevmClient, tx, logger, smokeTest.ReceiptTimeout)
-	if receipt.Status == 0 {
-		panic("approve failed")
-	}
+	fmt.Printf("eth zrc20 approve receipt: status %d\n", receipt.Status)
 
 	// Get current nonce on zevm for DeployerAddress - Need to keep track of nonce at client level
 	blockNum, err := smokeTest.ZevmClient.BlockNumber(context.Background())
@@ -215,7 +228,7 @@ func WithdrawCCtx(sm *runner.SmokeTestRunner) {
 	for {
 		select {
 		case <-ticker.C:
-			WithdrawBTCZRC20(sm)
+			WithdrawETHZRC20(sm)
 		}
 	}
 }
@@ -241,7 +254,6 @@ func EchoNetworkMetrics(sm *runner.SmokeTestRunner) {
 				ChainId: chainID.Int64(),
 			})
 			if err != nil {
-				fmt.Printf("error getting pending cctx: %s", err.Error())
 				continue
 			}
 			sends := cctxResp.CrossChainTx
@@ -251,7 +263,6 @@ func EchoNetworkMetrics(sm *runner.SmokeTestRunner) {
 			if len(sends) > 0 {
 				fmt.Printf("pending nonces %d to %d\n", sends[0].GetCurrentOutTxParam().OutboundTxTssNonce, sends[len(sends)-1].GetCurrentOutTxParam().OutboundTxTssNonce)
 			} else {
-				fmt.Printf("no pending cctx\n")
 				continue
 			}
 			//
@@ -290,43 +301,13 @@ func WithdrawETHZRC20(sm *runner.SmokeTestRunner) {
 		zevmNonce.Add(zevmNonce, big.NewInt(1))
 	}()
 
+	ethZRC20 := sm.ETHZRC20
+
 	sm.ZevmAuth.Nonce = zevmNonce
-	_, err := sm.ETHZRC20.Withdraw(sm.ZevmAuth, local.DeployerAddress.Bytes(), big.NewInt(100))
+	_, err := ethZRC20.Withdraw(sm.ZevmAuth, local.DeployerAddress.Bytes(), big.NewInt(100))
 	if err != nil {
 		panic(err)
 	}
-}
-
-func WithdrawBTCZRC20(sm *runner.SmokeTestRunner) {
-	defer func() {
-		zevmNonce.Add(zevmNonce, big.NewInt(1))
-	}()
-
-	sm.ZevmAuth.Nonce = zevmNonce
-	tx, err := sm.BTCZRC20.Withdraw(sm.ZevmAuth, local.DeployerAddress.Bytes(), big.NewInt(100))
-	if err != nil {
-		panic(err)
-	}
-	MonitorCCTXFromTxHash(sm, tx, zevmNonce.Int64())
-}
-
-func MonitorCCTXFromTxHash(sm *runner.SmokeTestRunner, tx *ethtypes.Transaction, nonce int64) {
-	receipt := utils.MustWaitForTxReceipt(sm.Ctx, sm.ZevmClient, tx, sm.Logger, sm.ReceiptTimeout)
-	if receipt.Status == 0 {
-		sm.Logger.Print("nonce %d: withdraw evm tx failed", nonce)
-		return
-	}
-	cctx := utils.WaitCctxMinedByInTxHash(sm.Ctx, tx.Hash().Hex(), sm.CctxClient, sm.Logger, sm.ReceiptTimeout)
-	if cctx.CctxStatus.Status != crosschaintypes.CctxStatus_OutboundMined {
-		sm.Logger.Print(
-			"nonce %d: withdraw cctx failed with status %s, message %s",
-			nonce,
-			cctx.CctxStatus.Status,
-			cctx.CctxStatus.StatusMessage,
-		)
-		return
-	}
-	sm.Logger.Print("nonce %d: withdraw cctx success", nonce)
 }
 
 // Get ETH based chain ID
