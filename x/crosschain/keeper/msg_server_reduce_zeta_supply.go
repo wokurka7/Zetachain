ed73c3127e4ac69a21cad6be9059abb08fb834f3
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2024-03-27 15:14:24 -0400

    add comments to the message server

--- x/crosschain/keeper/msg_server_reduce_zeta_supply.go
+++ x/crosschain/keeper/msg_server_reduce_zeta_supply.go
@@ -17,6 +17,10 @@ import (
 	observertypes "github.com/zeta-chain/zetacore/x/observer/types"
 )
 
+// ReduceZetaSupply reduces the Zeta supply on external chains
+// This is done by burning Zeta tokens on the external chain by sending it to the burn address.
+// The message uses the provided chainID, amount, and burn address to create a new CCTX. If a burn address is not provided, the default burn address is used.
+// This cctx can only be created by the policy account of group2
 func (k msgServer) ReduceZetaSupply(goCtx context.Context, msg *types.MsgReduceZetaSupply) (*types.MsgReduceZetaSupplyResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	if msg.Creator != k.zetaObserverKeeper.GetParams(ctx).GetAdminPolicyAccount(observertypes.Policy_Type_group2) {
@@ -51,6 +55,7 @@ func (k msgServer) ReduceZetaSupply(goCtx context.Context, msg *types.MsgReduceZ
 	return &types.MsgReduceZetaSupplyResponse{}, nil
 }
 
+// GetCmdCCTX returns a new CrossChainTx for of Cointype Cmd which is used to reduce the Zeta supply on external chains
 func GetCmdCCTX(ctx sdk.Context, chainID int64, amount sdkmath.Uint, tss observertypes.TSS, multipliedGasPrice string, burnAddress string) types.CrossChainTx {
 	height := ctx.BlockHeight()
 	index := GetIndexForReduceZetaSupplyCMD(chainID, height)
@@ -97,6 +102,7 @@ func GetCmdCCTX(ctx sdk.Context, chainID int64, amount sdkmath.Uint, tss observe
 	return cctx
 }
 
+// ProcessCmdCCTX updates the nonce for the given chainID and CCTX
 func (k Keeper) ProcessCmdCCTX(ctx sdk.Context, cctx types.CrossChainTx) error {
 	err := k.UpdateNonce(ctx, cctx.GetCurrentOutTxParam().ReceiverChainId, &cctx)
 	if err != nil {
@@ -105,10 +111,12 @@ func (k Keeper) ProcessCmdCCTX(ctx sdk.Context, cctx types.CrossChainTx) error {
 	return nil
 }
 
+// SaveCmdCCTX saves the given CCTX to the store
 func (k Keeper) SaveCmdCCTX(ctx sdk.Context, cctx types.CrossChainTx) {
 	k.SetCctxAndNonceToCctxAndInTxHashToCctx(ctx, cctx)
 }
 
+// GetIndexForReduceZetaSupplyCMD returns the index for the given chainID and blockHeight this is used to identify the CCTX for reducing the Zeta supply
 func GetIndexForReduceZetaSupplyCMD(chainID int64, blockHeight int64) string {
 	indexString := fmt.Sprintf("%d-%d", chainID, blockHeight)
 	hash := crypto.Keccak256Hash([]byte(indexString))
